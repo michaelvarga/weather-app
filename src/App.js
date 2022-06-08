312699d4c36b995f3b8b3e414e33b6ad9c5c09f3
Mike Varga <mikevarga07@gmail.com>
2022-06-07 20:22:28 -0500

    second commit. made the background dynamic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+
 const api = {
   key: "a7554c1e88ba66f0d735a50734399497",
   baseUrl: "https://api.openweathermap.org/data/2.5/",
@@ -21,7 +22,9 @@ function App() {
     }
   };
 
-  const displayDate = (newDate) => {
+  const displayDate = (timeOffset) => {
+    //finds the local date/time in city that user searches for
+    const newDate = new Date(new Date().getTime() + timeOffset * 1000 + new Date().getTimezoneOffset() * 60000);
     const months = [
       "January",
       "February",
@@ -46,16 +49,47 @@ function App() {
       "Saturday",
     ];
 
-    let day = days[newDate.getDay()];
-    let date = newDate.getDate();
-    let month = months[newDate.getMonth()];
-    let year = newDate.getFullYear();
+    const day = days[newDate.getDay()];
+    const date = newDate.getDate();
+    const month = months[newDate.getMonth()];
+    const year = newDate.getFullYear();
+    //set the time to display in 12hr format
+    const time = newDate
+      .toLocaleTimeString()
+      .replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
 
-    return `${day} ${month} ${date} ${year}`;
+    return `${day} ${month} ${date} ${year} ${time}`;
+  };
+
+  const weatherType = (id) => {
+    if (id === 800) {
+      return "clear";
+    } else if (id >= 300 && id <= 531) {
+      return "rain";
+    } else if (id >= 803 && id <= 804) {
+      return "overcast";
+    } else if (id >= 200 && id <= 232) {
+      return "storm";
+    } else if (id >= 600 && id <= 622) {
+      return "snow";
+    } else if (id >= 700 && id <= 781) {
+      return "hazy";
+    } else {
+      return "part-cloudy";
+    }
+  };
+
+  const backgroundClass = () => {
+    let time;
+    //check API for presence of 'd' or 'n' in id, indicating day/night
+    const isDay = weather.weather[0].icon.slice(-1) === 'd';
+    isDay ? time = 'day' : time = 'night'
+    const type = weatherType(weather.weather[0].id);
+    return `app ${type}-${time}`;
   };
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main !== "undefined") ? backgroundClass() : "app"}>
       <main>
         <div className="search-box">
           <input
@@ -70,12 +104,19 @@ function App() {
         {typeof weather.main != "undefined" ? (
           <div>
             <div className="location-container">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{displayDate(new Date())}</div>
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{displayDate(weather.timezone)}</div>
             </div>
             <div className="weather-container">
-              <div className="temperature">{Math.round(weather.main.temp)}&deg;F</div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="temperature">
+                {Math.round(weather.main.temp)}&deg;F
+              </div>
+              <div className="weather">
+                {weather.weather[0].description.toUpperCase()}
+              </div>
+              <img src={require(`./icons/${weather.weather[0].icon}.png`)} alt="weather icon"/>
             </div>
           </div>
         ) : (
